da459f1822833ea2dfb246dd8617922271d3a24a
Mhel Medina <rommel.d.medina@gmail.com>
2020-03-07 20:20:34 +0800

    Added feature to list countries with confirmed cases

--- src/main/java/com/medinar/covid19tracker/controller/Covid19Controller.java
+++ src/main/java/com/medinar/covid19tracker/controller/Covid19Controller.java
@@ -2,7 +2,11 @@ package com.medinar.covid19tracker.controller;
 
 import com.medinar.covid19tracker.model.LocationStats;
 import com.medinar.covid19tracker.service.Covid19Service;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,13 +23,21 @@ public class Covid19Controller {
     Covid19Service covid19Service;
     
     @GetMapping("/")
-    public String covid19(Model model) {
-         List<LocationStats> allStats = covid19Service.getAllStats();
+    public String covid19(Model model) throws Exception {
+        List<LocationStats> allStats = covid19Service.getLocationStats();
         int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
         int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalReportedCases);
         model.addAttribute("totalNewCases", totalNewCases);
        return "covid19";
+    }
+    
+    @GetMapping("/countries")
+    public String countries(Model model) throws Exception {
+        List<String> countries = covid19Service.getCountries();
+        model.addAttribute("totalCountries", countries.size());
+        model.addAttribute("countries", countries);
+        return "countries";
     }
 }
--- src/main/java/com/medinar/covid19tracker/service/Covid19Service.java
+++ src/main/java/com/medinar/covid19tracker/service/Covid19Service.java
@@ -8,13 +8,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import javax.annotation.PostConstruct;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 /**
@@ -24,35 +25,39 @@ import org.springframework.stereotype.Service;
 @Service
 public class Covid19Service {
 
-    private List<LocationStats> allStats = new ArrayList<>();
-
-    public List<LocationStats> getAllStats() {
-        return allStats;
+    public List<LocationStats> getLocationStats() throws IOException {
+        List<LocationStats> newStats = new ArrayList<>();
+        URL url = new URL(URL_COVID19_STATS);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream inputStream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
+        for (CSVRecord record : records) {
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            int latestCases = Integer.parseInt(record.get(record.size() - 1));
+            int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
+            newStats.add(locationStat);
+        }
+        return newStats;
     }
 
-    @PostConstruct
-    @Scheduled(cron = "* * 1 * * *")
-    public void getData() throws IOException {
-        try {
-            List<LocationStats> newStats = new ArrayList<>();
-            URL url = new URL(URL_COVID19_STATS);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            InputStream inputStream = connection.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
-            for (CSVRecord record : records) {
-                LocationStats locationStat = new LocationStats();
-                locationStat.setState(record.get("Province/State"));
-                locationStat.setCountry(record.get("Country/Region"));
-                int latestCases = Integer.parseInt(record.get(record.size() - 1));
-                int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
-                locationStat.setLatestTotalCases(latestCases);
-                locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
-                newStats.add(locationStat);
-            }
-            this.allStats = newStats;
-        } catch (UnknownHostException ex) {
-            System.err.println(ex);
+    public List<String> getCountries() throws IOException {
+        Set<String> countries = new HashSet<>();
+        URL url = new URL(URL_COVID19_STATS);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream inputStream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
+        for (CSVRecord record : records) {
+            countries.add(record.get("Country/Region"));
         }
+        
+        List<String> sortedCountries = countries.stream().collect(Collectors.toList());
+        Collections.sort(sortedCountries, (o1, o2) -> o1.compareTo(o2));
+        return sortedCountries;
     }
 }
