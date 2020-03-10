699fe96d184bd902be9ce3cc9005b2f756f750a9
a012001734 <a012001734@DW00231659.HO.AD.BDO>
2020-03-10 09:46:46 +0800

    Added a workaround that enables the app to read the stats from the classpath instead of the one from the web due to the UnkownHostException being encountered whenever the app is run using a proxy server.

--- src/main/java/com/medinar/covid19tracker/service/Covid19Service.java
+++ src/main/java/com/medinar/covid19tracker/service/Covid19Service.java
@@ -1,5 +1,8 @@
 package com.medinar.covid19tracker.service;
 
+import static com.medinar.covid19tracker.constant.Constants.FLD_COUNTRY_REGION;
+import static com.medinar.covid19tracker.constant.Constants.FLD_PROVINCE_STATE;
+import static com.medinar.covid19tracker.constant.Constants.PATH_COVID19_STATS;
 import static com.medinar.covid19tracker.constant.Constants.URL_COVID19_STATS;
 import com.medinar.covid19tracker.model.LocationStats;
 import java.io.BufferedReader;
@@ -8,6 +11,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -28,14 +32,26 @@ public class Covid19Service {
     public List<LocationStats> getLocationStats() throws IOException {
         List<LocationStats> newStats = new ArrayList<>();
         URL url = new URL(URL_COVID19_STATS);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        InputStream inputStream = connection.getInputStream();
+        HttpURLConnection connection;
+        InputStream inputStream;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            inputStream = connection.getInputStream();
+        }
+        catch (UnknownHostException ex) {
+            System.err.println("Switching to offline mode.");
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            inputStream = classloader.getResourceAsStream(PATH_COVID19_STATS);
+        }
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT
+                .withFirstRecordAsHeader()
+                .parse(reader);
         for (CSVRecord record : records) {
             LocationStats locationStat = new LocationStats();
-            locationStat.setState(record.get("Province/State"));
-            locationStat.setCountry(record.get("Country/Region"));
+            locationStat.setState(record.get(FLD_PROVINCE_STATE));
+            locationStat.setCountry(record.get(FLD_COUNTRY_REGION));
             int latestCases = Integer.parseInt(record.get(record.size() - 1));
             int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
             locationStat.setPreviousDayCases(prevDayCases);
@@ -54,27 +70,36 @@ public class Covid19Service {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
         for (CSVRecord record : records) {
-            countries.add(record.get("Country/Region"));
+            countries.add(record.get(FLD_COUNTRY_REGION));
         }
 
         List<String> sortedCountries = countries.stream().collect(Collectors.toList());
         Collections.sort(sortedCountries, (o1, o2) -> o1.compareTo(o2));
         return sortedCountries;
     }
-    
+
     public List<LocationStats> getDataByCountry(String country) throws IOException {
-        
+        List<LocationStats> locationStats = new ArrayList<>();
         URL url = new URL(URL_COVID19_STATS);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        InputStream inputStream = connection.getInputStream();
+        HttpURLConnection connection;
+        InputStream inputStream;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            inputStream = connection.getInputStream();
+        }
+        catch (UnknownHostException ex) {
+            System.err.println("Switching to offline mode.");
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            inputStream = classloader.getResourceAsStream(PATH_COVID19_STATS);
+        }
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
-        List<LocationStats> locationStats = new ArrayList<>();
         for (CSVRecord record : records) {
-            if (country.equals(record.get("Country/Region"))) {
+            if (country.equals(record.get(FLD_COUNTRY_REGION))) {
                 LocationStats locationStat = new LocationStats();
-                locationStat.setCountry(record.get("Country/Region"));
-                locationStat.setState(record.get("Province/State"));
+                locationStat.setCountry(record.get(FLD_COUNTRY_REGION));
+                locationStat.setState(record.get(FLD_PROVINCE_STATE));
                 int latestCases = Integer.parseInt(record.get(record.size() - 1));
                 int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
                 locationStat.setLatestCases(latestCases);
